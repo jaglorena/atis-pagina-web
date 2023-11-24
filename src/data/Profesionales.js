8e0ea36c0c4af9b0acc80889eafe434f3144eb99
Ivonne Duran <111211850+ivonne-duran@users.noreply.github.com>
2023-11-23 22:07:45 -0600

    Cambios en logica de asignacion de citas (#6)
    * Arreglando archivos y errores
    
    * Agregando disable a los botones
    
    * Cambios en UI
    
    * Cambios en agendar cita - logica
    
    * Arreglando mayusculas

--- src/data/Profesionales.js
+++ src/data/Profesionales.js
@@ -1,16 +1,16 @@
 const Profesionales = [
-    { nombre: "Dr. Miguel Estrada", especialidad: "Abogado" },
-    { nombre: "Dr. Manuel Hernandez", especialidad: "Abogado" },
-    { nombre: "Dr. Alejandro Hidalgo", especialidad: "Abogado" },
-    { nombre: "Dra. Maria Lugano", especialidad: "Abogado" },
-    { nombre: "Lic. Mirna Durango", especialidad: "Psicologo" },
-    { nombre: "Lic. Marcos Martinez", especialidad: "Psicologo" },
-    { nombre: "Lic. Miguel Montano", especialidad: "Psicologo" },
-    { nombre: "Dra. Luisa Magaña", especialidad: "Psicologo" },
-    { nombre: "Dr. Julio Vento", especialidad: "Psiquiatra" },
-    { nombre: "Dr. Juan Perez", especialidad: "Psiquiatra" },
-    { nombre: "Dra. Marta Casado", especialidad: "Psiquiatra" },
-    { nombre: "Dr. Kelvin Sierra", especialidad: "Psiquiatra" },
-]
+  { nombre: "Abg. Miguel Estrada", especialidad: "Abogado" },
+  { nombre: "Abg. Manuel Hernandez", especialidad: "Abogado" },
+  { nombre: "Abg. Alejandro Hidalgo", especialidad: "Abogado" },
+  { nombre: "Abgda. Maria Lugano", especialidad: "Abogado" },
+  { nombre: "Lic. Mirna Durango", especialidad: "Psicologo" },
+  { nombre: "Lic. Marcos Martinez", especialidad: "Psicologo" },
+  { nombre: "Lic. Miguel Montano", especialidad: "Psicologo" },
+  { nombre: "Dra. Luisa Magaña", especialidad: "Psicologo" },
+  { nombre: "Dr. Julio Vento", especialidad: "Psiquiatra" },
+  { nombre: "Dr. Juan Perez", especialidad: "Psiquiatra" },
+  { nombre: "Dra. Marta Casado", especialidad: "Psiquiatra" },
+  { nombre: "Dr. Kelvin Sierra", especialidad: "Psiquiatra" },
+];
 
-export default Profesionales
+export default Profesionales;
